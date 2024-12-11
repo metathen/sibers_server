edd9dfc57b867307ef883aca2906edfea77f577c
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-11 15:21:16 +0700

    function for create a channel

--- controllers/channel-controller.js
+++ controllers/channel-controller.js
@@ -2,7 +2,22 @@ const {prisma} = require('../prisma/prisma-client');
 
 const ChannelControllers = {
 	createChannel: async (req, res) => {
-		
+		const {name} = req.body;
+		const createdBy = req.user.userId;
+		if(!name) return res.status(400).json({error: "Invalid!"});
+		try {
+			const cahnnel = await prisma.channels.create({
+				data: {
+					name,
+					creatorId: createdBy,
+					members: [createdBy]
+				}
+			});
+			res.json(cahnnel);
+		} catch (error) {
+			console.error('Error with create a channel', error);
+			res.status(500).json({error: "server error"});
+		}
 	},
 	addUser: async () => {},
 	joinChannel: async () => {},
