e1b5cdf2388cba6e47183097664308d4e1db6d8c
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-11 15:22:05 +0700

    function for add a user

--- controllers/channel-controller.js
+++ controllers/channel-controller.js
@@ -19,7 +19,36 @@ const ChannelControllers = {
 			res.status(500).json({error: "server error"});
 		}
 	},
-	addUser: async () => {},
+	addUser: async (req, res) => {
+		const { channelId, userId } = req.body;
+		if (!channelId || !userId) return res.status(400).json({ error: "Channel ID and User ID are required" });
+	  
+		try {
+			const channel = await prisma.channels.findUnique({
+				where: { id: channelId },
+			});
+		
+			if (!channel) return res.status(404).json({ error: "Channel not found" });
+
+			//check user role
+			// if (channel.creatorId !== req.user.userId) return res.status(403).json({ error: "Only the channel creator can add members" });
+		
+			// Check if the user is already a member of the channel.
+			if (channel.members.includes(userId)) return res.status(400).json({ error: "User is already a member of this channel" });
+		
+			const updatedChannel = await prisma.channels.update({
+				where: { id: channelId },
+				data: {
+				members: {push: userId}
+				},
+			});
+		
+			res.json(updatedChannel);
+		} catch (error) {
+		  	console.error('Error adding user to channel', error);
+		  	res.status(500).json({ error: "Server error" });
+		}
+	},
 	joinChannel: async () => {},
 	deleteUser: async () => {}
 }
