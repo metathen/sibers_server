2c6a32e0b3718d72438e832b57337e45208f268c
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-12-11 12:26:31 +0700

    register controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,29 @@
+const { prisma } = require("../prisma/prisma-client");
+const bcrypt = require('bcryptjs');
+
 const UserController = {
-	register: async () => {},
+	register: async (req, res) => {
+		const {username, password} = req.body;
+
+		if(!username || !password) {
+			return res.status(400).json({error: "Register Invalid!"});
+		}
+
+		try {
+			const existUser = await prisma.user.findUnique(({where: {username}}));
+			if(existUser) {
+				return res.status(400).json({error: "Username has been declared"});
+			}
+			const hashPass = await bcrypt.hash(password, 10);
+			const user = await prisma.user.create({
+				data: {username, password: hashPass}
+			});
+			res.json(user);
+		} catch (error) {
+			console.error('Error in register', error);
+			res.status(500).json({error: 'Interna; server error'});
+		}
+	},
 	login: async () => {},
 	getUserById: async () => {},
 	updateUser: async () => {},
